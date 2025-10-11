a3390dfdccceb2fac124acf4cf6e7c9c80ba5185
Aeithne <40723680+Aeithne@users.noreply.github.com>
2025-10-11 12:05:41 -0300

    Create helloworld2.cbl

---
+++ aula01/helloworld2.cbl
@@ -0,0 +1,27 @@
+      ******************************************************************
+      * Author: AEITHNE
+      * Date: 11/10/2025
+      * Purpose: Aprendizado de Cobol.
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION. 
+       PROGRAM-ID. HELLO-WORLD-COMNOME. 
+       AUTHOR. AEITHNE. 
+       ENVIRONMENT DIVISION. 
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+           01 WS-MENSAGEM PIC X(20) VALUE SPACES.
+           01 WS-NOME PIC X(20) VALUE SPACES.
+       PROCEDURE DIVISION.
+       PRIMEIRA-SECAO.
+       PRIMEIRO-PARAG.
+           MOVE 'Como vai mundo!' TO WS-MENSAGEM. 
+           DISPLAY WS-MENSAGEM
+           DISPLAY 'Por favor, digite seu nome:'.
+           ACCEPT WS-NOME
+           DISPLAY 'Prazer em conhecê-lo,' WS-NOME. 
+           MOVE 'Tenha um ótimo dia' TO WS-MENSAGEM. 
+           DISPLAY WS-MENSAGEM
+           STOP RUN.
+       END PROGRAM HELLO-WORLD-COMNOME.
+       
